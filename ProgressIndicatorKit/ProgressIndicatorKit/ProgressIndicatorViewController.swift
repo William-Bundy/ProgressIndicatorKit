c2cd268f73bc52825c9b34d7f813e46f8cc33724
William Bundy <williambundy@icloud.com>
2018-09-19 21:42:07 -0700

    Something basic working

--- ProgressIndicatorKit/ProgressIndicatorKit/ProgressIndicatorViewController.swift
+++ ProgressIndicatorKit/ProgressIndicatorKit/ProgressIndicatorViewController.swift
@@ -9,7 +9,64 @@
 import Foundation
 import UIKit
 
-class ProgressIndicatorViewController:UIViewController
+open class ProgressIndicatorViewController:UIViewController
 {
+	var graphic:UIView!;
+	var progress:Progress?
+	private(set) var isAnimating:Bool = false
+
+	public func followProgress(_ progress:Progress)
+	{
+		self.progress = progress
+		progress.addObserver(self, forKeyPath: "completedUnitCount", options: .new, context:nil)
+	}
+
+	open override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?)
+	{
+		if let progress = self.progress, keyPath == "completedUnitCount" {
+			let p = Double(progress.completedUnitCount) / Double(progress.totalUnitCount)
+			updateAnimationPercent(p)
+		}
+	}
+
+
+	public func updateAnimationPercent(_ percent:Double)
+	{
+
+	}
+
+	override open func viewDidLoad()
+	{
+		super.viewDidLoad()
+		graphic = UIView(frame: CGRect(x:100, y:100, width:100, height:100))
+		graphic.backgroundColor = .black
+		self.view.addSubview(graphic)
+	}
+
+	public func startAnimating()
+	{
+		if !isAnimating {
+			isAnimating = true
+			UIView.animate(withDuration: 1.0, delay: 0.5, options: .repeat, animations: {
+				self.graphic.transform = self.graphic.transform.rotated(by: 3.14 / 2)
+			}) { success in
+				print(success)
+			}
+		}
+	}
+
+	public func stopAnimating()
+	{
+		if isAnimating {
+			isAnimating = false
+			graphic.layer.anim
+		}
+	}
+
+	open func whenAllAnimationsFinished()
+	{
+
+	}
+
 
 }
--- ProgressIndicatorKitExample/ProgressIndicatorKitExample/ViewController.swift
+++ ProgressIndicatorKitExample/ProgressIndicatorKitExample/ViewController.swift
@@ -7,14 +7,23 @@
 //
 
 import UIKit
+import ProgressIndicatorKit
 
-class ViewController: UIViewController {
+class ViewController: ProgressIndicatorViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		super.startAnimating()
+	}
+
+	@IBAction func finishedLoading(_ sender: Any) {
+		super.stopAnimating()
+	}
 
 }
 
